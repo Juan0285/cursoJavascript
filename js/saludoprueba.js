f8cfb30327098501156f666bc15611b15b4cedfe
Juan Rojas <rojasj884@gmail.com>
2022-03-26 23:44:19 -0300

    clase 6_Rojas

--- js/saludoprueba.js
+++ js/saludoprueba.js
@@ -6,25 +6,42 @@ const prestamo = document.getElementById ("prestamo")
 
 const salario = document.getElementById ("salario")
 
-const ul = document.getElementById ("listado");
+const listado = document.getElementById ("listado");
 
 const boton = document.getElementById ("boton");
 
+const limpiar = document.getElementById ("limpiar");
 
 
 
+const guardarInfo = ()=> {
+        const datosForm = {nombre:"" , prestamo:0}
+        datosForm.nombre = nombre.value;
+        datosForm.prestamo = prestamo.value;
+        newLi = document.createElement("li");
+        newLi.append(datosForm.nombre , datosForm.prestamo);
+        listado.append(newLi)
+        localStorage.setItem("persona1" , JSON.stringify(datosForm));
+        console.log("se han guardado los datos correctamente");
+    }
+    
+    
+    boton.addEventListener ("click", guardarInfo);
 
 
-const agragarAlListado = ()=>{
-        const agregarItem = document.createElement("li");
-        agregarItem.className = "colorRojo";
-        agregarItem.innerText = nombre;
-        ul.append(agregarItem);
+    
+const limpiarDatos = ()=>{
+        while (listado) {
+            listado.removeChild(listado.lastChild);
+          }
+    }
+    
+    limpiar.addEventListener ("click", limpiarDatos);
+    
 
+const obtenerInfo = ()=>{
+        const recuperados = JSON.parse(localStorage.getItem("persona1"))
+        return recuperados
 }
 
-
-
-
-boton.addEventListener("click" , agragarAlListado)
-
+console.log(obtenerInfo())
